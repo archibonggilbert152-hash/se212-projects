1a9f1f762ef42f04ee709442cdc9548615a682db
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2026-02-04 14:15:47 -0800

    changes to week 5

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -16,16 +16,21 @@ public static class Recursion
     }
 
     // --------------------------------------------------
-    // PROBLEM 2: Permutations Choose
+    // PROBLEM 2: Permutations Choose (PUBLIC ENTRY)
     // --------------------------------------------------
     public static List<string> PermutationsChoose(char[] letters, int size)
     {
         List<string> results = new();
-        Permute("", letters, size, results);
+        PermutationsChoose("", letters, size, results);
         return results;
     }
 
-    private static void Permute(string prefix, char[] letters, int size, List<string> results)
+    // 👇 THIS is the overload the tests expect (3 args)
+    public static void PermutationsChoose(
+        string prefix,
+        char[] letters,
+        int size,
+        List<string> results)
     {
         if (prefix.Length == size)
         {
@@ -39,15 +44,19 @@ public static class Recursion
                 .Where((c, index) => index != i)
                 .ToArray();
 
-            Permute(prefix + letters[i], remaining, size, results);
+            PermutationsChoose(prefix + letters[i], remaining, size, results);
         }
     }
 
     // --------------------------------------------------
-    // PROBLEM 3: Climbing Stairs (Memoization)
+    // PROBLEM 3: Climbing Stairs (DEFAULT PARAM FIX)
     // --------------------------------------------------
-    public static int CountWaysToClimb(int s, Dictionary<int, int> remember)
+    public static int CountWaysToClimb(
+        int s,
+        Dictionary<int, int>? remember = null)
     {
+        remember ??= new Dictionary<int, int>();
+
         if (s < 0)
             return 0;
 
@@ -81,5 +90,35 @@ public static class Recursion
 
         WildcardBinary(pattern[..index] + "0" + pattern[(index + 1)..], results);
         WildcardBinary(pattern[..index] + "1" + pattern[(index + 1)..], results);
+    }
+
+    // --------------------------------------------------
+    // PROBLEM 5: Solve Maze (TEST EXPECTS THIS HERE)
+    // --------------------------------------------------
+    public static void SolveMaze(
+        Maze maze,
+        int x,
+        int y,
+        List<(int, int)> currPath,
+        List<string> results)
+    {
+        if (!maze.IsValidMove(currPath, x, y))
+            return;
+
+        currPath.Add((x, y));
+
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+            currPath.RemoveAt(currPath.Count - 1);
+            return;
+        }
+
+        SolveMaze(maze, x + 1, y, currPath, results);
+        SolveMaze(maze, x - 1, y, currPath, results);
+        SolveMaze(maze, x, y + 1, currPath, results);
+        SolveMaze(maze, x, y - 1, currPath, results);
+
+        currPath.RemoveAt(currPath.Count - 1);
     }
 }
